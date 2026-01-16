ddb31169ad289e6c4914a21c98e0d6f85cf5674e
posadist-revolution <ihunickesmith@gmail.com>
2026-01-15 20:31:14 -0700

    Revert "Release 0.1.3"

--- scripts/zip.js
+++ scripts/zip.js
@@ -6,21 +6,13 @@ import archiver from 'archiver';
 // Constants
 const BUILD_DIR = 'build';
 
-// Check for filename from input (optional)
-let filename = process.argv[2];
-
 // Get latest tag from git
-if (!filename) {
-    const tag = child_process.execSync('git describe --tags --abbrev=0').toString().trim();
-
-    console.log(`Creating zip for tag: ${tag}`);
-
-    // Set filename
-    filename = `cosmere-advanced-encounters-${tag}.zip`;
-}
+const tag = child_process.execSync('git describe --tags --abbrev=0').toString().trim();
 
 // Create a file to stream archive data to
-const output = fs.createWriteStream(filename);
+const output = fs.createWriteStream(
+    `cosmere-advanced-encounters-${tag}.zip`
+);
 
 // Create a new archive
 const archive = archiver('zip', {
@@ -32,8 +24,6 @@ archive.pipe(output);
 
 // Add files to the archive
 archive.directory(BUILD_DIR, false);
-
-console.log(`Writing zip file to: ${output.path}`);
 
 // Finalize the archive
 archive.finalize();
