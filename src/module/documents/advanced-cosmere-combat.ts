95ec823ed132de7765365eb1252bde040059ba71
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-01-17 14:22:52 -0700

    Renamed combatant_actions to .ts file

--- src/module/documents/advanced-cosmere-combat.ts
+++ src/module/documents/advanced-cosmere-combat.ts
@@ -1,5 +1,5 @@
 import { activeCombat, Dictionary } from "@src/index";
-import { CombatantActions } from "./combatant_actions.mjs";
+import { CombatantActions } from "./combatant_actions.js";
 import { CosmereCombatant } from "@src/declarations/cosmere-rpg/documents/combatant";
 import { getModuleSetting, RefreshCombatantActionsWhenOptions, SETTINGS } from "../settings";
 
