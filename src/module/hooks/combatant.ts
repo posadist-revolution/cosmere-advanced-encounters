64a93a38695b75bdfd6546c16f3e078aaec37357
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-02-15 11:06:12 -0700

    Fixed blocking actions when no combat has been started

--- src/module/hooks/combatant.ts
+++ src/module/hooks/combatant.ts
@@ -17,6 +17,9 @@ export function activateCombatantHooks(){
         item: CosmereItem,
         options: CosmereItem.UseOptions
     ) => {
+        if(!activeCombat.combat.started){
+            return true;
+        }
         // Check the settings for what level of control the module has over using actions
         let checkActionUsability = getModuleSetting(SETTINGS.CHECK_ACTION_USABILITY);
 
@@ -42,7 +45,7 @@ export function activateCombatantHooks(){
                 }
             }
         }
-        if(!getModuleSetting(SETTINGS.PULL_ACTIONS_FROM_CHAT)){
+        if(!(getModuleSetting(SETTINGS.PULL_ACTIONS_FROM_CHAT) && activeCombat.combat.started)){
             return true;
         }
         switch(item.system.activation.cost.type){
